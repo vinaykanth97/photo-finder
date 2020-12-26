ce877f4f1c77b62d92204391c1ae3d1923e6c624
vinaypump <vinaykennedy@gmail.com>
2020-12-26 23:02:32 +0530

    First push

---
+++ app.js
@@ -0,0 +1,79 @@
+const auth = "563492ad6f9170000100000185b19bdbd1054727b602a4850f6809bb";
+const searchBar = document.querySelector(".form-input");
+const searchForm = document.querySelector(".search-form");
+const gallery = document.querySelector(".gallery");
+//   "https://api.pexels.com/v1/search?query=nature&per_page=1"
+const moreBtn = document.querySelector(".more-btn .btn");
+let searchValue;
+let i = 15;
+let fetchLink;
+searchBar.addEventListener("input", searchStuffs);
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+async function searchStuffs(e) {
+  searchValue = e.target.value;
+}
+
+moreBtn.addEventListener("click", (e) => {
+  fetchMore(e);
+});
+async function fetchApi(url) {
+  let fetchPhoto = await fetch(`${url}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: auth,
+    },
+  });
+  let pexelData = await fetchPhoto.json();
+
+  return pexelData;
+}
+
+function generateData(data) {
+  data.photos.forEach((photos) => {
+    let gallerImg = document.createElement("div");
+    gallerImg.innerHTML = `
+    <div class="gallery-info">
+        <p>${photos.photographer}</p>
+        <a href="${photos.src.original}" target="_blank">View Original</a>
+    </div>
+    <img src="${photos.src.large}"/>
+    `;
+
+    gallerImg.classList.add("gallery-img");
+
+    gallery.appendChild(gallerImg);
+  });
+}
+
+async function getPhotos() {
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+  let pexelData = await fetchApi(fetchLink);
+  generateData(pexelData);
+}
+async function searchPhotos(query) {
+  clear();
+  fetchLink = `https://api.pexels.com/v1/search?query=${query}&per_page=15`;
+  let pexelData = await fetchApi(fetchLink);
+
+  generateData(pexelData);
+}
+
+async function fetchMore(e) {
+  i += 2;
+  let moreData = await fetchApi(
+    `https://api.pexels.com/v1/curated?per_page=${i}&page=1`
+  );
+  generateData(moreData);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchBar.value = "";
+}
+getPhotos();
